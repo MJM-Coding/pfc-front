65ab978ff653b915a056ae726bd163349d3eedb1
Jessica_Moscato <j.mos@live.fr>
2024-12-19 15:33:47 +0100

    test

--- src/components/itemList/itemList.tsx
+++ src/components/itemList/itemList.tsx
@@ -12,12 +12,12 @@ interface ItemListProps<T> {
 const ItemList = <T,>({ items, renderItem, isLoading }: ItemListProps<T>) => {
   // Afficher le message de chargement pendant que les données sont en cours de récupération
   if (isLoading) {
-    return <p className="loading">Chargement des éléments...</p>; // Message pendant le chargement
+    return <p className="loading">Chargement des animaux...</p>; // Message pendant le chargement
   }
 
   // Si les données sont chargées et la liste est vide, afficher "Aucun élément trouvé"
   if (items.length === 0) {
-    return <p className="no-items">Aucun élément trouvé</p>;
+    return <p className="no-items">Aucun animal trouvé</p>;
   }
 
   return <ul className="item-list">{items.map(renderItem)}</ul>;
