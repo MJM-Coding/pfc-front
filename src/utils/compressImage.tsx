0b252078d5acb7ce017288ee35a872c233138375
Jessica_Moscato <j.mos@live.fr>
2024-12-31 10:29:09 +0100

    compression des images avant envoi au back

--- src/utils/compressImage.tsx
+++ src/utils/compressImage.tsx
@@ -2,15 +2,15 @@ import imageCompression from "browser-image-compression";
 
 export const compressImage = async (file: File): Promise<File> => {
   const options = {
-    maxSizeMB: 1,
-    maxWidthOrHeight: 1024,
+    maxSizeMB: 0.5, // Réduisez à 0.5 MB
+    maxWidthOrHeight: 800, // Réduisez à 800 pixels
     useWebWorker: true,
   };
 
   try {
     const compressedFile = await imageCompression(file, options);
-    console.log("Taille initiale :", file.size / 1024, "Ko");
-    console.log("Taille compressée :", compressedFile.size / 1024, "Ko");
+    console.log("Taille initiale :", file.size / 800, "Ko");
+    console.log("Taille compressée :", compressedFile.size / 800, "Ko");
     return compressedFile;
   } catch (error) {
     console.error("Erreur lors de la compression :", error);
